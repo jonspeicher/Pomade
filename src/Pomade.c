7c0c1625ca01a505f047c7332073014a96021696
Jon Speicher <jon.speicher@gmail.com>
2013-08-11 22:57:12 -0400

    Change version, app and window name

--- src/Pomade.c
+++ src/Pomade.c
@@ -2,23 +2,20 @@
 #include "pebble_app.h"
 #include "pebble_fonts.h"
 
-
 #define MY_UUID { 0x78, 0x1D, 0x21, 0x66, 0x09, 0x09, 0x4F, 0x9C, 0x88, 0xFD, 0x89, 0x9B, 0x04, 0xBF, 0x5E, 0x32 }
+
 PBL_APP_INFO(MY_UUID,
-             "Template App", "Your Company",
-             1, 0, /* App version */
+             "Pomade", "Jon Speicher",
+             0, 1, /* App version */
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
 Window window;
 
-
 void handle_init(AppContextRef ctx) {
-
-  window_init(&window, "Window Name");
+  window_init(&window, "Pomade");
   window_stack_push(&window, true /* Animated */);
 }
-
 
 void pbl_main(void *params) {
   PebbleAppHandlers handlers = {
