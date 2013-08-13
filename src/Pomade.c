85c156bf03bcf2142cc82099d32a7a51391259d9
Jon Speicher <jon.speicher@gmail.com>
2013-08-12 22:58:10 -0400

    Add an action bar to the main window

--- src/Pomade.c
+++ src/Pomade.c
@@ -14,6 +14,7 @@ PBL_APP_INFO(MY_UUID,
 
 Window window;
 TextLayer timer_layer;
+ActionBarLayer action_bar;
 
 AppTimerHandle timer;
 #define POMODORO_COOKIE 1
@@ -21,6 +22,14 @@ AppTimerHandle timer;
 #define POMODORO_TICK_PERIOD_SEC POMODORO_TICK_PERIOD_MS / 1000
 
 static Pomodoro pomodoro;
+
+void window_load(Window* window) {
+  action_bar_layer_add_to_window(&action_bar, window);
+}
+
+void window_unload(Window* window) {
+  action_bar_layer_remove_from_window(&action_bar);
+}
 
 void handle_timer(AppContextRef ctx, AppTimerHandle handle, uint32_t cookie) {
   if (cookie == POMODORO_COOKIE) {
@@ -39,7 +48,12 @@ void handle_init(AppContextRef ctx) {
   pomodoro_init(&pomodoro);
 
   window_init(&window, "Pomade");
-  window_stack_push(&window, true /* Animated */);
+  window_set_window_handlers(&window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+
+  action_bar_layer_init(&action_bar);
 
   text_layer_init(&timer_layer, window.layer.frame);
   text_layer_set_text_alignment(&timer_layer, GTextAlignmentCenter);
@@ -47,6 +61,7 @@ void handle_init(AppContextRef ctx) {
   text_layer_set_text(&timer_layer, pomodoro.time_left_string);
   layer_add_child(&window.layer, &timer_layer.layer);
 
+  window_stack_push(&window, true /* Animated */);
   timer = app_timer_send_event(ctx, POMODORO_TICK_PERIOD_MS, POMODORO_COOKIE);
 }
 
