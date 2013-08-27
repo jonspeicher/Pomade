0df4f3e6c605c3644e1194145d2812c50f08c02b
Jon Speicher <jon.speicher@gmail.com>
2013-08-27 01:45:57 -0400

    Add some comments

--- src/pomodoro.c
+++ src/pomodoro.c
@@ -1,2 +1,8 @@
+// ----------------------------------------------------------------------------
+// pomodoro - Defines a model to track progress through the Pomodoro Technique
+// Copyright (c) 2013 Jonathan Speicher (jon.speicher@gmail.com)
+// Licensed under the MIT license: http://opensource.org/licenses/MIT
+// ----------------------------------------------------------------------------
+
 #include "pomodoro.h"
 
--- src/pomodoro.h
+++ src/pomodoro.h
@@ -1,4 +1,13 @@
+// ----------------------------------------------------------------------------
+// pomodoro - Defines a model to track progress through the Pomodoro Technique
+// Copyright (c) 2013 Jonathan Speicher (jon.speicher@gmail.com)
+// Licensed under the MIT license: http://opensource.org/licenses/MIT
+// ----------------------------------------------------------------------------
+
 #pragma once
+
+// Defines a structure type to hold the pomodoro technique segments.
 
 typedef struct {
 } Pomodoro;
+
