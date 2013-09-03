858ed0ab8b89977ff1264327f16c1f53db7c8a31
Jon Speicher <jon.speicher@gmail.com>
2013-09-03 02:00:52 -0400

    Clean up code, rename variables for display of pomodoro completion stuff

--- src/segment_view.c
+++ src/segment_view.c
@@ -10,24 +10,24 @@
 #include "pomodoro_config.h"
 #include "segment_view.h"
 
+// Define a variable to hold the count of pomodoros completed.
+
+static unsigned int pomodoros_completed;
+
 // Define the various user interface elements comprising this view.
 
-static TextLayer segment_type_text_layer;
-static Layer pomodoro_count_layer;
+static TextLayer break_layer;
+static Layer pomodoro_layer;
 
 // Define a variable to hold the previous unload handler for chaining.
 
 static WindowHandler previous_unload_handler;
 
-// Define a variable to hold the count of pomodoros completed.
-
-static unsigned int pomodoros_completed;
-
 // Private functions.
 
 static void load_and_add_view(Window* window);
 static void remove_and_unload_view(Window* window);
-static void update_pomodoro_count_layer(Layer* layer, GContext* ctx);
+static void update_pomodoro_layer(Layer* layer, GContext* ctx);
 
 // Public functions -----------------------------------------------------------
 
@@ -44,14 +44,8 @@ void segment_view_set_pomodoros_completed(unsigned int completed) {
 }
 
 void segment_view_show_segment_type(PomodoroSegmentType type) {
-  layer_set_hidden(&segment_type_text_layer.layer, (type != POMODORO_SEGMENT_TYPE_BREAK));
-  layer_set_hidden(&pomodoro_count_layer, (type != POMODORO_SEGMENT_TYPE_POMODORO));
-  // TBD: Make this a lookup table - JRS 8/30
-  if (type == POMODORO_SEGMENT_TYPE_POMODORO) {
-    text_layer_set_text(&segment_type_text_layer, "pomodoro");
-  } else if (type == POMODORO_SEGMENT_TYPE_BREAK) {
-    text_layer_set_text(&segment_type_text_layer, "break");
-  }
+  layer_set_hidden(&break_layer.layer, (type != POMODORO_SEGMENT_TYPE_BREAK));
+  layer_set_hidden(&pomodoro_layer, (type != POMODORO_SEGMENT_TYPE_POMODORO));
 }
 
 // Private functions ----------------------------------------------------------
@@ -59,25 +53,26 @@ void segment_view_show_segment_type(PomodoroSegmentType type) {
 void load_and_add_view(Window* window) {
   unsigned int width = window->layer.frame.size.w - ACTION_BAR_WIDTH;
 
-  text_layer_init(&segment_type_text_layer, GRect(0, 90, width, 40));
-  text_layer_set_text_alignment(&segment_type_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(&segment_type_text_layer,
-    fonts_get_system_font(FONT_KEY_GOTHIC_28));
-  layer_add_child(&window->layer, &segment_type_text_layer.layer);
+  layer_init(&pomodoro_layer, GRect(0, 90, width, 40));
+  layer_set_update_proc(&pomodoro_layer, update_pomodoro_layer);
+  layer_add_child(&window->layer, &pomodoro_layer);
 
-  layer_init(&pomodoro_count_layer, GRect(0, 90, width, 40));
-  layer_set_update_proc(&pomodoro_count_layer, update_pomodoro_count_layer);
-  layer_add_child(&window->layer, &pomodoro_count_layer);
+  text_layer_init(&break_layer, GRect(0, 90, width, 40));
+  text_layer_set_text_alignment(&break_layer, GTextAlignmentCenter);
+  text_layer_set_font(&break_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+  text_layer_set_text(&break_layer, "break");
+  layer_add_child(&window->layer, &break_layer.layer);
 }
 
 void remove_and_unload_view(Window* window) {
-  layer_remove_from_parent(&segment_type_text_layer.layer);
+  layer_remove_from_parent(&pomodoro_layer);
+  layer_remove_from_parent(&break_layer.layer);
   if (previous_unload_handler) {
     previous_unload_handler(window);
   }
 }
 
-void update_pomodoro_count_layer(Layer* layer, GContext* ctx) {
+void update_pomodoro_layer(Layer* layer, GContext* ctx) {
   unsigned int span = layer->frame.size.w / (POMODORO_COUNT_FOR_LONG_BREAK + 1);
 
   graphics_context_set_stroke_color(ctx, GColorBlack);
